028cca89caf19f59c11f101861bd8c2d43ddb5ac
Eric Moreno <moreno.eric01@gmail.com>
2019-08-06 18:38:41 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
    
    #when user makes GET to /users/:user_id/profile/new
    def new
-      # Render blank prfile details form 
+      # Render blank profile details form 
+      @profile = Profile.new
    end
-    
 end
