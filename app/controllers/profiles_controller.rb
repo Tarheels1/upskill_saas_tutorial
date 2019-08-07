92708abce185896ea547ca248655f56b7ac406ca
Eric Moreno <moreno.eric01@gmail.com>
2019-08-07 17:06:10 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,9 @@
 class ProfilesController < ApplicationController
    
+   before_action :authenticate_user!
+   before_action :only_current_user
+   
+   
    #when user makes GET to /users/:user_id/profile/new
    def new
       # Render blank profile details form 
@@ -9,13 +13,13 @@ class ProfilesController < ApplicationController
    # POST to /user/:user_id/profile
    def create
      # Ensure that we have the user who is filling out form
-     @user = User.find(params[id: :user_id])
+     @user = User.find(params[:user_id])
      
      # Create profile linked to this specific user
      @profile = @user.build_profile(profile_params)
      if @profile.save
         flash[:success] = "Profile updated!"
-        redirect_to user_path(params[:user_id])
+        redirect_to user_path(id: params[:user_id])
      else
         render action: :new
      end
@@ -46,5 +50,10 @@ class ProfilesController < ApplicationController
    private
       def profile_params
          params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+      end
+      
+      def only_current_user
+         @user = User.find(params[:user_id])
+         redirect_to(root_url) unless @user == current_user
       end
 end
